fef97ebae105b954df38a78419701c216375259c
ChristopherMWilliamsCLC <christophermwilliams@stu.clcillinois.edu>
2025-03-01 21:55:19 -0600

    Add files via upload
    3/1/25

---
+++ pokemon.cpp
@@ -0,0 +1,48 @@
+#include "pokemon.h"
+
+pokemon::pokemon()
+{
+	name = "";
+	gender = "";
+}
+
+pokemon::pokemon(string name = "", string gender = "")
+{
+	name = name;
+	gender = gender;
+}
+
+void pokemon::printPokemon()
+{
+	// Print pokemon name and gender.
+	cout << "Pokemon: " << name << endl;
+	cout << "Gender: " << gender << endl;
+
+	// Print pokemon nature.
+	printNature();
+	cout << endl;
+
+	// Print pokemon stats.
+	printStats();
+	cout << "_________________________________" << endl << endl;
+}
+
+void pokemon::setName(string pokeName)
+{
+	name = pokeName;
+}
+
+void pokemon::setGender(string pokeGender)
+{
+	gender = pokeGender;
+}
+
+string pokemon::getName()
+{
+	return name;
+}
+
+string pokemon::getGender()
+{
+	return gender;
+}
